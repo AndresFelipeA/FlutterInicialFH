e71fc9ea0d23ff63eb03261784fdbf2ec651425f
SP-2 andres <andres5585@gmail.com>
2021-07-22 16:51:52 -0500

    Future Builder y manejo de Json

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:ferhcourse/src/providers/menu_provider.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
@@ -15,7 +16,51 @@ class HomePage extends StatelessWidget {
     );
   }
 
-  _lista() {
-    return ListView();
+  Widget _lista() {
+    //print(menuProvider.opciones);
+    //menuProvider.cargarData()
+
+    return FutureBuilder(
+      future: menuProvider.cargarData(),
+      initialData: [],
+      builder: (context, AsyncSnapshot<List<dynamic>> snapshot) {
+        // print('builder');
+        // print(snapshot.data);
+        return ListView(
+          children: _listaItems(snapshot.data),
+        );
+      },
+    );
+
+    // return ListView(
+    //   children: _listaItems(),
+    // );
+  }
+
+  //pilas si arroja un error de List dynamic es por el null safety y hay que hacerlo apcional el parámetro
+  List<Widget> _listaItems(List<dynamic>? data) {
+    final List<Widget> opciones = [];
+
+    //otro error de null safety, la data hay que tomarla como si no pudiera llefar con el ?
+    data?.forEach((opt) {
+      final widgetTemp = ListTile(
+        title: Text(opt['texto']),
+        leading: Icon(
+          Icons.ac_unit_outlined,
+          color: Colors.blue,
+        ),
+        trailing: Icon(Icons.keyboard_arrow_right, color: Colors.blue),
+        onTap: () {},
+      );
+
+      opciones
+        ..add(widgetTemp)
+        ..add(Divider(
+          color: Colors.black,
+          height: 5,
+        ));
+    });
+
+    return opciones;
   }
 }
